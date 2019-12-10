d92af792da70c485ae4918a983b03113981dcc74
Brantley Jones <brantleyjones74@gmail.com>
2019-12-10 15:27:44 -0600

    getting a response of game id's back from api

---
+++ game-journal/Controllers/APIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace game_journal.Controllers
+{
+    public class APIController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public APIController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/games");
+            var client = _clientFactory.CreateClient("igdb");
+            var response = await client.SendAsync(request);
+            string name = await response.Content.ReadAsStringAsync();
+            return View();
+        }
+    }
+}
